102e7ad8bc961324d6a8e80b8b7a2c309a9dbe28
MuazulHasanKhan <muazulhasankhan@gmail.com>
2026-08-18 16:50:17 +0530

    working on deployment

--- app/cache/redis_cache.py
+++ app/cache/redis_cache.py
@@ -1,11 +1,15 @@
 import json
 import redis
 from app.core.config import settings
-
-redis_client = redis.Redis.from_url(settings.REDIS_URL)
+import os
+from dotenv import load_dotenv
+# redis_client = redis.Redis.from_url(settings.REDIS_URL)
 
 # two functions assuming that similar query was asked earlier
 
+REDIS_URL = os.getenv('REDIS_URL')
+
+redis_client = redis.StrictRedis.from_url(REDIS_URL, decode_responses=True)
 
 def get_cached_pediction(key:str):
     value = redis_client.get(key)
@@ -15,4 +19,4 @@ def get_cached_pediction(key:str):
     return None
 
 def set_cached_prediction(key:str, value:dict, expiry: int ):
-    redis_client.setex(key, expiry, json.dums(value))
+    redis_client.setex(key, expiry, json.dumps(value))
