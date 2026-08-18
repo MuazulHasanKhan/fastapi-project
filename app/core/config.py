289f68c6d660cadf6d4a0b115b87918ab2e1a467
MuazulHasanKhan <muazulhasankhan@gmail.com>
2026-08-18 13:16:03 +0530

    added containerization code

--- app/core/config.py
+++ app/core/config.py
@@ -13,6 +13,6 @@ class Settings:
     JWT_SECRET_KEY: str = os.getenv('JWT_SECRET_KEY', 'secret')
     JWT_ALGORITHM: str = 'HS256'
     REDIS_URL: str = os.getenv('REDIS_URL', 'redis://localhost:6379')
-    MODEL_PATH: str = 'app/models/model.pkl'
+    MODEL_PATH: str = 'app/models/model.joblib'
 
 settings = Settings()
